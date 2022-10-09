ee00e91990cbfd6cf2454056c19ec5139d7cb400
Iamvictorsam <iamvictorsam@gmail.com>
2022-10-10 00:03:15 +0100

    added label

--- src/edges.js
+++ src/edges.js
@@ -4,5 +4,5 @@ export default [
   { id: "el2-3", source: "2", target: "3", animated: true },
   { id: "el2-4", source: "2", target: "4", animated: true },
   { id: "el2-5", source: "2", target: "5", animated: true },
-  { id: "el6-1", source: "1", target: "6", animated: false },
+  { id: "el6-1", source: "1", target: "6", label: "Back to Input" },
 ];
